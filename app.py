cd191ab843746247479f1491d31e13ba40b71e8b
Tim Thompson <tim.thompson@capgemini.com>
2020-02-28 16:53:58 +0000

    stuff

--- app.py
+++ app.py
@@ -12,9 +12,13 @@ db = client.test_db
 street = db.street
 street.drop()
 
-# Open file with context manager
-with open('data/met/2017-01/2017-01-metropolitan-street.csv') as csv_file:
+# Input variables
+input_type = "crimeType"
+maxDistance = 5000
+data_dir = "data"
 
+# Open file with context manager
+with open("data/met/2017-01/2017-01-metropolitan-street.csv") as csv_file:
     # Use dict reader to read each line from csv
     reader = list(csv.DictReader(csv_file))
 
@@ -31,59 +35,64 @@ with open('data/met/2017-01/2017-01-metropolitan-street.csv') as csv_file:
             lat = float(row["Latitude"])
         except ValueError as ve:
             lat = 0
+
         data = {
             "crimeId": row["Crime ID"],
             "month": row["Month"],
             "reportedBy": row["Reported by"],
             "fallsWithin": row["Falls within"],
-            "location": {
-                "type": "Point",
-                "coordinates": [long, lat]
-            },
+            "location": {"type": "Point", "coordinates": [long, lat]},
             "locationName": row["Location"],
             "lsoaCode": row["LSOA code"],
             "lsoaName": row["LSOA name"],
             "crimeType": row["Crime type"],
             "lastOutcomeCategory": row["Last outcome category"],
-            "context": row["Context"]
+            "context": row["Context"],
         }
         street_list.append(data)
 
     street.insert_many(street_list)
     street.create_index([("location", pymongo.GEOSPHERE)])
 
+group_by = {
+    "crimeType": {"crimeType": "$crimeType"},
+    "outcome:": {"lastOutcomeCategory": "$lastOutcomeCategory"},
+}
+
+check_exists = {}
+
 # Define pipeline for aggregating data
 aggregation_pipeline = [
     {"$match": {"crimeType": {"$exists": True, "$ne": ""}}},
     {"$match": {"lastOutcomeCategory": {"$exists": True, "$ne": ""}}},
-    {"$group": {
-        "_id": {
-            "crimeType": "$crimeType",
-            "lastOutcomeCategory": "$lastOutcomeCategory"
-        },
-        "count": {
-            "$sum": 1
+    {
+        "$group": {
+            "_id": {
+                "crimeType": "$crimeType",
+                "lastOutcomeCategory": "$lastOutcomeCategory",
+            },
+            "count": {"$sum": 1},
         }
-    }},
-    {"$sort": {"count": -1}}
+    },
+    {"$sort": {"count": -1}},
 ]
 
 geo_aggregation_pipeline = [
     {
         "$geoNear": {
-            "near": {
-                "type": "Point", "coordinates": [0.140634, 51.583427]
-            },
+            "near": {"type": "Point", "coordinates": [0.140634, 51.583427]},
             "spherical": True,
             "distanceField": "calcDistance",
-            "maxDistance": 500
+            "maxDistance": maxDistance,
         }
-    }
+    },
+    {"$match": {input_type: {"$exists": True, "$ne": ""}}},
+    {"$group": {"_id": group_by.get(input_type), "count": {"$sum": 1}}},
+    {"$sort": {"count": -1}},
 ]
 
 # Run aggregation and display results
 result = street.aggregate(geo_aggregation_pipeline)
 pprint(list(result))
-
 
 # pprint(street.find_one())
