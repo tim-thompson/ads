d3e5aeb1c4e3f1c9814a668f497661cc73295b23
Tim Thompson <tim.thompson@capgemini.com>
2020-02-28 16:35:21 +0000

    stuff

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import pymongo
 from pymongo import MongoClient
 from pprint import pprint
 import csv
@@ -10,41 +11,46 @@ db = client.test_db
 # Clear and create collection
 street = db.street
 street.drop()
-outcome = db.outcome
-outcome.drop()
-
-# Reference for street level data csv headers
-headers = ["crimeId", "month", "reportedBy", "fallsWithin", "longitude", "latitude", "location", "lsdaCode", "lsdaName",
-           "crimeType", "lastOutcomeCategory", "context"]
 
 # Open file with context manager
 with open('data/met/2017-01/2017-01-metropolitan-street.csv') as csv_file:
+
     # Use dict reader to read each line from csv
-    reader = csv.DictReader(csv_file, fieldnames=headers)
+    reader = list(csv.DictReader(csv_file))
 
-    # Insert read dict direct into mongo
-    street.insert_many(reader)
+    street_list = []
 
-with open('data/met/2017-01/2017-01-metropolitan-outcomes.csv') as csv_file:
-    reader = csv.DictReader(csv_file)
-    outcome.insert_many(reader)
+    # Construct data model from csv data using geospatial constructs
+    for row in reader:
+        try:
+            long = float(row["Longitude"])
+        except ValueError as ve:
+            long = 0
 
-join_pipeline = [
-    {
-        "$lookup": {
-            "from": "outcome",
-            "localField": "crimeId",
-            "foreignField": "Crime ID",
-            "as": "outcome"
+        try:
+            lat = float(row["Latitude"])
+        except ValueError as ve:
+            lat = 0
+        data = {
+            "crimeId": row["Crime ID"],
+            "month": row["Month"],
+            "reportedBy": row["Reported by"],
+            "fallsWithin": row["Falls within"],
+            "location": {
+                "type": "Point",
+                "coordinates": [long, lat]
+            },
+            "locationName": row["Location"],
+            "lsoaCode": row["LSOA code"],
+            "lsoaName": row["LSOA name"],
+            "crimeType": row["Crime type"],
+            "lastOutcomeCategory": row["Last outcome category"],
+            "context": row["Context"]
         }
-    },
-    {
-        "$out": "street_outcome"
-    }
-]
+        street_list.append(data)
 
-result = street.aggregate(join_pipeline)
-pprint(list(result))
+    street.insert_many(street_list)
+    street.create_index([("location", pymongo.GEOSPHERE)])
 
 # Define pipeline for aggregating data
 aggregation_pipeline = [
@@ -62,13 +68,22 @@ aggregation_pipeline = [
     {"$sort": {"count": -1}}
 ]
 
+geo_aggregation_pipeline = [
+    {
+        "$geoNear": {
+            "near": {
+                "type": "Point", "coordinates": [0.140634, 51.583427]
+            },
+            "spherical": True,
+            "distanceField": "calcDistance",
+            "maxDistance": 500
+        }
+    }
+]
+
 # Run aggregation and display results
-result = street.aggregate(aggregation_pipeline)
-# pprint(list(result))
+result = street.aggregate(geo_aggregation_pipeline)
+pprint(list(result))
 
-# Construct results collection
-results = db.results
-results.drop()
 
-result_list = list(result)
-# results.insert_many(list(result))
+# pprint(street.find_one())
